ee5a6ed92c1e0aaac66063021588c6abc8f691c2
Thorsten Knöller <thorsten.knoeller@immundiagnostik.com>
2018-09-18 12:59:40 +0200

    EDIT/db: database configuration by environment variables

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Translation API written in Go.
 ## Routes
 
 You can check out the different routes of the API [here](ROUTES.md).
+
+## Running
+
+This project is configured to run inside docker. The database connection can be configured using the following environment variables:
+* DATABASE_TYPE (mysql or postgres)
+* DATABASE_HOST
+* DATABASE_PORT
+* DATABASE_NAME
+* DATABASE_USER
+* DATABASE_PASSWORD
+
+There should be a docker image in the gitlab registry.
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 	"preventis.io/translationApi/model"
 	"preventis.io/translationApi/routing"
 )
 
 func main() {
-	db, err := gorm.Open("mysql", "translation:translation@/translation?charset=utf8&parseTime=True&loc=Local")
+	dbDialect := getEnv("DATABASE_TYPE", "mysql")
+	dbArgs := getDbArgs(dbDialect)
+
+	db, err := gorm.Open(dbDialect, dbArgs)
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -16,4 +22,26 @@ func main() {
 
 	model.InitDB(db)
 	routing.StartRouter()
+}
+
+func getDbArgs(dbDialect string) string {
+	dbHost := getEnv("DATABASE_HOST", "localhost")
+	dbPort := getEnv("DATABASE_PORT", "3306")
+	dbUser := getEnv("DATABASE_USER", "translation")
+	dbPassword := getEnv("DATABASE_PASSWORD", "translation")
+	dbDatabase := getEnv("DATABASE_NAME", "translation")
+
+	if dbDialect == "mysql" {
+		return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbDatabase)
+	} else if dbDialect == "postgres" {
+		return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", dbHost, dbPort, dbUser, dbDatabase, dbPassword)
+	}
+	return ""
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
